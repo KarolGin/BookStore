50a6bb486ef3cbe77aa3371ef9355a02f6d15bbf
ypayel <kukankovartem@gmail.com>
2024-06-09 15:28:04 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Background } from './components/Nav/Background/Background';
