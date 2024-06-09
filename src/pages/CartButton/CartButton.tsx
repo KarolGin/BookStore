d07fc14ce29cf8e8c5702fda9b2b8b681e9076f9
ypayel <kukankovartem@gmail.com>
2024-06-09 13:49:37 +0200

    update + steps in BasketBook

--- src/pages/CartButton/CartButton.tsx
+++ src/pages/CartButton/CartButton.tsx
@@ -2,16 +2,16 @@
 import "./CartButton.scss"
 import { Link } from "react-router-dom";
 export const CartBook = () => {
-    return (
-        <>
-        <Link to="/search">
-       <div className="cart">
-      <button className="cart-button">
-        My basket
-        <span className="shopping-cart">🛒</span>
-      </button>
-    </div>
-    </Link>
-        </>
-    )
+  return (
+    <>
+      <Link to="/search">
+        <div className="cart">
+          <button className="cart-button">
+            My basket
+            <img src="/images/favicon-32x32.png" alt="Shopping Cart" className="shopping-cart" />
+          </button>
+        </div>
+      </Link>
+    </>
+  )
 }
