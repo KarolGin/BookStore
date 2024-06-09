6f14fb9c656ebbd884178464da89dac45074327e
Karol G <gintyllokarol@gmail.com>
2024-06-09 12:49:46 +0200

    Navigation on the mainPage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,11 @@
 import { BookList } from "../../components/BookList/BookList";
-import { LinkForAddProduct } from "../LinksForPages/AddProductButton";
 import SearchInput from "../../pages/SearchInput/searchInput";
+import { MainNav } from "../Nav/MainNav/MainNav";
 export const MainPage = () => {
   return (
     <div>
+      <MainNav />
       <SearchInput />
-      <LinkForAddProduct />
       <BookList />
     </div>
   );
---
+++ src/components/Nav/MainNav/MainNav.tsx
@@ -0,0 +1,21 @@
+import "./MainNav.scss";
+
+export const MainNav = () => {
+  return (
+    <div className="main-nav">
+        <span className="container-for-links">
+      <ul className="main-nav-list">
+        <li className="main-nav-list-item">
+          <a href="/">Strona główna</a>
+        </li>
+        <li className="main-nav-list-item">
+          <a href="/add">Dodaj produkt</a>
+        </li>
+        <li className="main-nav-list-item">
+          <a href="/edit">Edytuj produkt</a>
+        </li>
+      </ul>
+        </span>
+    </div>
+  );
+};
