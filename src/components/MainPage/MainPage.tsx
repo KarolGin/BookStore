f7deb013c46f2774c066f2c4224ec1bf88a9f7ad
Karol G <gintyllokarol@gmail.com>
2024-06-15 10:07:34 +0200

    Navigation and hamburgerMenu

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,10 @@
 import { BookList } from "../../components/BookList/BookList";
 import SearchInput from "../../pages/SearchInput/searchInput";
+import { Background } from "../Nav/Background/Background";
 import { MainNav } from "../Nav/MainNav/MainNav";
 export const MainPage = () => {
   return (
-    <div>
+    <div><Background />
       <MainNav />
       <SearchInput />
       <BookList />
