a3c236b993b111b95796cebc789c85c15bdb24c4
Karol G <gintyllokarol@gmail.com>
2024-06-15 11:04:18 +0200

    format files

--- src/components/Nav/MainNav/MainNav.tsx
+++ src/components/Nav/MainNav/MainNav.tsx
@@ -3,19 +3,19 @@ import "./MainNav.scss";
 export const MainNav = () => {
   return (
     <div className="main-nav">
-        <span className="container-for-links">
-      <ul className="main-nav-list">
-        <li className="main-nav-list-item">
-          <a href="/">Strona główna</a>
-        </li>
-        <li className="main-nav-list-item">
-          <a href="/add">Dodaj produkt</a>
-        </li>
-        <li className="main-nav-list-item">
-          <a href="/edit">Edytuj produkt</a>
-        </li>
-      </ul>
-        </span>
+      <span className="container-for-links">
+        <ul className="main-nav-list">
+          <li className="main-nav-list-item">
+            <a href="/">Strona główna</a>
+          </li>
+          <li className="main-nav-list-item">
+            <a href="/add">Dodaj produkt</a>
+          </li>
+          <li className="main-nav-list-item">
+            <a href="/edit">Edytuj produkt</a>
+          </li>
+        </ul>
+      </span>
     </div>
   );
 };
