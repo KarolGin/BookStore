53003ef7bc796d3b501de7316f8bb62072f267d9
Karol G <gintyllokarol@gmail.com>
2024-06-15 11:04:18 +0200

    format files

--- src/components/Nav/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/Nav/HamburgerMenu/HamburgerMenu.tsx
@@ -2,22 +2,26 @@ import { useState } from "react";
 import "./HamburgerMenu.scss";
 
 export const HamburgerMenu = () => {
-
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-        
-    };
-    return (
-              <div>
-            <button className="hamburger" onClick={toggleMenu}>
-                &#9776;
-            </button>
-            <ul className={`menu ${isOpen ? 'open' : ''}`}>
-                <li><a href="/">Strona Główna</a></li>
-                <li><a href="/add">Dodaj</a></li>
-                <li><a href="/edit">Edytuj</a></li>
-            </ul>
-        </div>
-    );
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <button className="hamburger" onClick={toggleMenu}>
+        &#9776;
+      </button>
+      <ul className={`menu ${isOpen ? "open" : ""}`}>
+        <li>
+          <a href="/">Strona Główna</a>
+        </li>
+        <li>
+          <a href="/add">Dodaj</a>
+        </li>
+        <li>
+          <a href="/edit">Edytuj</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
